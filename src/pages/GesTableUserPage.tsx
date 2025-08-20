aac6959e1416ae482903aab53826dde63300ff47
marmar1115ri <mariaricotrujillo1115@gmail.com>
2025-08-20 14:49:08 -0500

    modficacion para la conexion de la tabla areas listar

--- src/pages/GesTableUserPage.tsx
+++ src/pages/GesTableUserPage.tsx
@@ -1,21 +1,27 @@
+
 import GesTablesUser from "../components/atomic/templates/GesTablesUser";
+
+
 import { useAreas } from '../hooks/Areas/useAreas'; 
+
+
 
 export default function GesTableUserPage() {
 
   const { areas, isLoading, isError } = useAreas();
 
-  
+
   if (isLoading) {
     
     return <div>Cargando datos...</div>;
   }
 
-  
+
   if (isError) {
     return <div>Error: No se pudieron cargar las áreas.</div>;
   }
 
+ 
   return (
     <>
       <GesTablesUser areasData={areas} />
